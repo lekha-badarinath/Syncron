e547fd4c87013bb9284f2c3152d91fcd0e47e231
lekha-badarinath <34691945+lekha-badarinath@users.noreply.github.com>
2019-05-30 20:44:11 +0530

    30-05-2019 Added capital output in the assignment

--- UI_Learning/Office_Assignments/02LoggingInput/script.ts
+++ UI_Learning/Office_Assignments/02LoggingInput/script.ts
@@ -3,7 +3,8 @@
 let logInput = function(){
     let text = document.getElementById('input')
     console.log(text.value)
-    document.getElementById('displayInput').innerHTML = text.value
+    document.getElementById('displayOne').innerHTML = text.value
+    document.getElementById('displayTwo').innerHTML = text.value.toUpperCase()
 }
 
 

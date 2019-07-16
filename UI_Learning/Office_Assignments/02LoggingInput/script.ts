eb548aedb8631f1b7c5be533b6c4eebf4f273062
lekha-badarinath <34691945+lekha-badarinath@users.noreply.github.com>
2019-07-16 11:03:02 +0530

    16-07-2019

--- UI_Learning/Office_Assignments/02LoggingInput/script.ts
+++ UI_Learning/Office_Assignments/02LoggingInput/script.ts
@@ -1,10 +1,15 @@
 //Logging input
 
 let logInput = function(){
-    let text = document.getElementById('input')
-    console.log(text.value)
-    document.getElementById('displayOne').innerHTML = text.value
-    document.getElementById('displayTwo').innerHTML = text.value.toUpperCase()
-}
+    let text = document.getElementById('input');
+    console.log(text.value);
+    document.getElementById('displayOne').innerHTML = text.value;
+    document.getElementById('displayTwo').innerHTML = text.value.toUpperCase();
+};
+
+let logMessage = function(){
+    let text = document.getElementById('ip');
+    document.getElementById('displayThree').innerHTML = "Hello, "+text.value;
+};
 
 
